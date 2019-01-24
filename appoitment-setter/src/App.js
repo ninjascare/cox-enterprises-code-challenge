79a732d0d6377a321737583d80a74a69a8db0bcc
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2019-01-24 12:09:59 -0500

    got my modals going for the time table

--- appoitment-setter/src/App.js
+++ appoitment-setter/src/App.js
@@ -1,86 +1,164 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./App.css";
+import styled from "styled-components";
+
+const TimeTable = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  .timeslot {
+    border: 1px solid black;
+    width: 30%;
+    text-align: center;
+  }
+`;
 
 class App extends Component {
+  state = {
+    timeslot: "",
+    newUser: {
+      name: "",
+      number: ""
+    },
+    blankUser: {
+      name: "",
+      number: ""
+    },
+    availableTimes: [
+      "9 a.m.",
+      "10 a.m.",
+      "11 a.m.",
+      "12 p.m.",
+      "1 p.m.",
+      "2 p.m.",
+      "3 p.m.",
+      "4 p.m.",
+      "5 p.m."
+    ]
+    // timebooked = false;
+  };
+
+  activateModal = e => {
+    e.target.style.backgroundColor = "red";
+    let value = e.target.innerHTML;
+    this.setState({ timeslot: value });
+  };
+
+  handleInput = e => {
+    const updatedNewUser = { ...this.state.newUser };
+    updatedNewUser[e.target.name] = e.target.value;
+    this.setState({ newUser: updatedNewUser });
+  };
+
+  clearInput = e => {
+    let elements = document.querySelectorAll(".timeslot");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.backgroundColor = "white";
+    }
+    this.setState({ newUser: this.state.blankUser });
+  };
+
+  handleSubmit = e => {
+    // this.setState(timebooked: )
+  };
+
   render() {
+    const returnTimes = this.state.availableTimes.map((timeslot, i) => {
+      return (
+        <div
+          key={i}
+          data-toggle="modal"
+          data-target="#exampleModalCenter"
+          onClick={this.activateModal}
+          className="timeslot"
+        >
+          "{timeslot}"
+        </div>
+      );
+    });
     return (
-      <div className="App">
-        <div className="jumbotron jumbotron-fluid">
-          <h1>
-            Welcome! <br /> please choose a time slot to arrive for your
-            appointment
-          </h1>
-          <div class="form-group">
-            <label for="exampleFormControlSelect2">
-              Choose appointment time
-            </label>
-            <br />
-            <select
-              multiple
-              className="form-control"
-              id="exampleFormControlSelect2"
-              style={{
-                width: 700
-              }}
-            >
-              <option data-toggle="modal" data-target="#exampleModalCenter">
-                9:00 am
-              </option>
-              <option>10:00 am</option>
-              <option>11:00 am</option>
-              <option>12:00 pm</option>
-              <option>1:00 pm</option>
-              <option>2:00 pm</option>
-              <option>3:00 pm</option>
-              <option>4:00 pm</option>
-              <option>5:00 pm</option>
-            </select>
-          </div>
-          <div
-            className="modal fade"
-            id="exampleModalCenter"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalCenterTitle"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog modal-dialog-centered" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalCenterTitle">
-                    Modal title
-                  </h5>
-                  <button
-                    type="button"
-                    className="close"
-                    data-dismiss="modal"
-                    aria-label="Close"
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <input type="text" />
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                  <button type="button" className="btn btn-primary">
-                    Save changes
-                  </button>
-                </div>
+      <div className="board">
+        <TimeTable>
+          <div>{returnTimes}</div>
+        </TimeTable>
+
+        {/* Modal Code for Boot-Strap */}
+        <div
+          className="modal fade"
+          id="exampleModalCenter"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalCenterTitle"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalCenterTitle">
+                  Please enter your name and number to confirm your{" "}
+                  {this.state.timeslot} appointment
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span onClick={this.clearInput} aria-hidden="true">
+                    &times;
+                  </span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <input
+                  onChange={this.handleInput}
+                  value={this.state.newUser.name}
+                  name="name"
+                  type="text"
+                  placeholder="Your Name"
+                />
+              </div>
+              <div className="modal-body">
+                <input
+                  onChange={this.handleInput}
+                  value={this.state.newUser.number}
+                  name="number"
+                  type="text"
+                  placeholder="Your Number"
+                />
+              </div>
+              <div className="modal-footer">
+                <button
+                  onClick={this.clearInput}
+                  type="button"
+                  className="btn btn-primary"
+                  data-dismiss="modal"
+                >
+                  Cancel All Appointments
+                </button>
+                <button
+                  onClick={this.handleSubmit}
+                  type="button"
+                  className="btn btn-primary"
+                  data-dismiss="modal"
+                >
+                  Book Apppointment
+                </button>
               </div>
             </div>
           </div>
         </div>
+        {/* End Modal Code from Boot-Strap */}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  timeslot: state.timeslot
+});
+
+export default connect(mapStateToProps)(App);
